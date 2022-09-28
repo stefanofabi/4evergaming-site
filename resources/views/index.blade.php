bd532ea69b6e3ee79c95c5fa7d6ba68569bb0164
Stefano <stefanofabi96@gmail.com>
2022-09-28 13:10:23 -0300

    Add text for carousel images when viewing on small screens

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -93,13 +93,16 @@
             <a href="https://clientes.4evergaming.com.ar/store/counter-strike?currency=2"> 
               <img loading="lazy" src="{{ asset('images/carousel/cs-wallpaper-01.jpg') }}" class="d-block img-fluid" alt="Jugador terrorista recargando el arma" height="400">
             </a>
+
             <div class="carousel-caption">
               <h3>Protección Anticheat y Mitigación DDoS</h3>
               <p> Redes confiables y ultra rápidas sobre los carriers más importantes y reconocidos de la región </p>
 
-              <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-16-300fps?currency=2" class="btn btn-danger" > 300 FPS </a>
-              <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-16-1000fps?currency=2" class="btn btn-success"> 1000 FPS </a>
-              <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-16-500fps?currency=2" class="btn btn-warning"> 500 FPS </a>
+              <div class="d-none d-md-block">
+                <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-16-300fps?currency=2" class="btn btn-danger" > 300 FPS </a>
+                <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-16-1000fps?currency=2" class="btn btn-success"> 1000 FPS </a>
+                <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-16-500fps?currency=2" class="btn btn-warning"> 500 FPS </a>
+              </div>
             </div>
           </div>
 
@@ -107,12 +110,15 @@
             <a href="https://clientes.4evergaming.com.ar/store/counter-strike?currency=2">
               <img loading="lazy" src="{{ asset('images/carousel/csgo-wallpaper-01.jpeg') }}" class="d-block img-fluid" alt="Jugador Terrorista tomando la posicion a otro jugador Policia" height="400">
             </a>
-            <div class="carousel-caption d-none d-md-block">
-              <h3> Sin contratos, cancelá en cualquier momento. </h3>
-              <p> Sin Compromisos, ni contratos. Facturado cada mes. Sin costos ocultos. Servicio con garantía de reembolso </p>
-            
-              <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-global-offensive-64-tickrate?currency=2" class="btn btn-danger" > 64 Tick</a>
-              <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-global-offensive-128-tickrate?currency=2" class="btn btn-success"> 128 Tick </a>
+
+            <div class="carousel-caption">
+              <h3> Sin contratos, cancelá en cualquier momento </h3>
+              <p> Sin Compromisos. Facturado cada mes. Sin costos ocultos.</p>
+
+              <div class="d-none d-md-block">
+                <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-global-offensive-64-tickrate?currency=2" class="btn btn-danger" > 64 Tick</a>
+                <a href="https://clientes.4evergaming.com.ar/store/counter-strike/counter-strike-global-offensive-128-tickrate?currency=2" class="btn btn-success"> 128 Tick </a>
+              </div>
             </div>
           </div>
 
@@ -121,12 +127,14 @@
               <img loading="lazy" src="{{ asset('images/carousel/mta-wallpaper-01.jpg') }}" class="d-block img-fluid" alt="Gangster disparando por la ventana del auto" height="400">
             </a>
 
-            <div class="carousel-caption d-none d-md-block">
+            <div class="carousel-caption">
               <h3> Recursos sin límites </h3>
               <p> Tu Comunidad crece y necesitas más... perfecto!!! te ayudamos a reconfigurar tu servidor o migrar a otro hardware </p>
-            
-              <a href="https://clientes.4evergaming.com.ar/store/grand-theft-auto/san-andreas-multi-player-samp?currency=2" class="btn btn-success" > GTA San Andreas</a>
-              <a href="https://clientes.4evergaming.com.ar/store/grand-theft-auto/multi-theft-auto-mta?currency=2" class="btn btn-success"> MTA San Andreas </a>
+
+              <div class="d-none d-md-block">
+                <a href="https://clientes.4evergaming.com.ar/store/grand-theft-auto/san-andreas-multi-player-samp?currency=2" class="btn btn-success" > GTA San Andreas</a>
+                <a href="https://clientes.4evergaming.com.ar/store/grand-theft-auto/multi-theft-auto-mta?currency=2" class="btn btn-success"> MTA San Andreas </a>
+              </div>
             </div>
           </div>
         </div>
