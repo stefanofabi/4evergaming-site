f9690499aafe2471bbfbbe077cb892cc37060df5
Stefano <stefanofabi96@gmail.com>
2022-09-28 02:49:17 -0300

    Add payment option with automatic debit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -355,7 +355,7 @@
               <a class="text-decoration-none text-dark" href="{{ asset('transferencia-cbu-brubank.pdf') }}" target="_blank"> 
                 Transferencia CBU 
               </a> 
-                <span class="badge text-bg-danger"> 10% descuento </span> 
+              <span class="badge text-bg-danger"> 10% descuento </span> 
             </li>
             
             <li class="list-group-item"> 
@@ -387,7 +387,19 @@
                   <path d="M12 9h2V8h-2v1Z"/>
                 </svg>
                 Escanear QR 
-              </a> 
+              </a>
+              <span class="badge text-bg-danger"> 5% descuento </span>  
+            </li>
+
+            <li class="list-group-item"> 
+              <a class="text-decoration-none text-dark" href="https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c938084837e83a40183822347fc0318" target="_blank"> 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+                <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+                <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+              </svg>
+                Débito automático 
+              </a>
+              <span class="badge text-bg-danger"> 20% descuento </span>  
             </li>
 
             <li class="list-group-item"> 
