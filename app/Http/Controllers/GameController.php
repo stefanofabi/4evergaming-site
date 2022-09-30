65a7eb1787453985df7ef9d6122b96af292b32ea
Stefano <stefanofabi96@gmail.com>
2022-09-29 23:26:05 -0300

    Validate input parameters for /api/games

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,39 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Exception;
+
 class GameController extends Controller
 {
     //
 
     public function getGameState(Request $request)
     {
-        $type = $request->game;
-        $ip = $request->ip;
-        $port = $request->port;
-
-        $GameQ = new \GameQ\GameQ();
-
-        $GameQ->addServer([
-            'type' => $type,
-            'host' => $ip . ':' . $port,
-        ]);
-
-        $GameQ->addFilter('secondstohuman');
-        $results = $GameQ->process();
-
-        foreach ($results as $id => $var) {
-            $players = $var['players'];
-            $players = collect($var['players'])->sortBy('score')->reverse()->toArray();
-            unset(
-                $var['players'],
-            );
-            $var['ip'] = $ip;
-
-            return array([
-                'id' => $id,
-                'var' => $var,
-                'players' => $players,
+        try {
+            $request->validate([
+                'game' => 'required',
+                'ip' => 'required|ip',
+                'port' => 'required|numeric|min:0|max:65535',
             ]);
+
+            $ip = $request->ip . ':' . $request->port;
+
+            $GameQ = new \GameQ\GameQ();
+
+            $GameQ->addServer([
+                'type' => $request->game,
+                'host' => $ip,
+            ]);
+
+            $GameQ->addFilter('secondstohuman');
+            $results = $GameQ->process();
+
+            foreach ($results as $id => $var) {
+                $players = $var['players'];
+                $players = collect($var['players'])->sortBy('score')->reverse()->toArray();
+                unset(
+                    $var['players'],
+                );
+                $var['ip'] = $ip;
+
+                return array([
+                    'id' => $id,
+                    'var' => $var,
+                    'players' => $players,
+                ]);
+            }
+        } catch (Exception $e) {
+            return response()->json(['errors' => $e->getMessage()]);
         }
     }
 }
