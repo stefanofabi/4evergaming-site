0382e9de2ccfb60b6cb0a2de637288e822fdb4ed
Stefano <stefanofabi96@gmail.com>
2022-09-30 01:15:13 -0300

    Add button refresh for games ranking

--- resources/views/pages/index/offcanvas/counter_strike16.blade.php
+++ resources/views/pages/index/offcanvas/counter_strike16.blade.php
@@ -1,6 +1,13 @@
       <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasCounterStrike16" aria-labelledby="offcanvasCounterStrike16Label">
         <div class="offcanvas-header">
-          <h5 class="offcanvas-title"> Counter-Strike 1.6</h5>
+          <h5 class="offcanvas-title"> Counter-Strike 1.6 
+            <button class="btn btn-secondary ms-3" onclick="loadGame('cs16')"> 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+              </svg> 
+            </button>
+          </h5>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         
