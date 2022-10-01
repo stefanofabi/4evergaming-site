675a4221d4208f8903b84d1d78b0a7b865f9ce5f
Stefano <stefanofabi96@gmail.com>
2022-10-01 03:59:03 -0300

    Add Google Analytics

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -46,6 +46,16 @@
                 });
             });
         </script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71638662-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-71638662-1');
+        </script>
         
         @include('tawkto')
 
