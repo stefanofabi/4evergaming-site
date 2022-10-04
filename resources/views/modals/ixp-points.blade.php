772fe22ec2b03602b2be0d87d961a4fe3f437ce2
Stefano <stefanofabi96@gmail.com>
2022-10-04 13:28:17 -0300

    Increase modal width

--- resources/views/modals/ixp-points.blade.php
+++ resources/views/modals/ixp-points.blade.php
@@ -1,21 +1,23 @@
 <!-- Modal -->
-<div class="modal fade" id="IxpPointsModal" tabindex="-1" aria-labelledby="IxpPointsModalLabel" aria-hidden="true">
+<div class="modal modal-lg fade" id="IxpPointsModal" tabindex="-1" aria-labelledby="IxpPointsModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="IxpPointsModalLabel">Puntos de Intercambio de Tráfico – IXPs</h1>
+        <h1 class="modal-title fs-5" id="IxpPointsModalLabel">Puntos de Intercambio de Tráfico</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <p> 
-            Los IXPs son espacios donde los proveedores de acceso a Internet, los organismos de gobierno, las entidades académicas, los grandes proveedores de contenido y otros actores del ecosistema de Internet, se interconectan entre sí para intercambiar el tráfico de sus redes de una manera más eficiente, con el objetivo de acercar el contenido al usuario final.  Al estar más cerca, mejora sensiblemente la experiencia del usuario y la velocidad de navegación al reducir el tiempo de viaje del contenido (latencia).
+            Los Puntos de Intercambio de Tráfico (IXPs, por sus siglas en inglés) son espacios donde los proveedores de acceso a Internet, los organismos de gobierno, las entidades académicas, los grandes proveedores de contenido y otros actores del ecosistema de Internet, se interconectan entre sí para intercambiar el tráfico de sus redes de una manera más eficiente, con el objetivo de acercar el contenido al usuario final.  Al estar más cerca, mejora sensiblemente la experiencia del usuario y la velocidad de navegación al reducir el tiempo de viaje del contenido (latencia).
         </p>
 
-        <img loading="lazy" class="img-fluid" src="{{ asset('images/cabase-ixps.png') }}">
-
-        <p class="mt-3">
+        <div class="text-center">
+          <img loading="lazy" class="img-fluid" src="{{ asset('images/cabase-ixps.png') }}">
+        
+          <p class="mt-3">
             Mirá el mapa completo en este <a href="{{ asset('cabase-ixps.pdf') }}" target="_blank"> link </a>
-        </p>
+          </p>
+        </div>
       </div>
 
       <div class="modal-footer">
