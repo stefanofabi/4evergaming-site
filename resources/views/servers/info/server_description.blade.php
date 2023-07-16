3cf298c6c1ebbc1bff289cf32029c455db08e0cf
Stefano <stefanofabi96@gmail.com>
2023-07-15 23:52:24 -0300

    Hide server description when is empty

--- resources/views/servers/info/server_description.blade.php
+++ resources/views/servers/info/server_description.blade.php
@@ -1,4 +1,4 @@
-<div class="card">    
+<div class="card @if (empty($server->description)) d-none @endif">    
     <div class="card-header">
         <div> <h4 class="text-center fw-bold"> Descripción </h4> </div>
     </div>    
