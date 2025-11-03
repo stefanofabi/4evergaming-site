78c05cc14d2bc2b4fb0004b2f4e1c3008f0518fa
Stefano <stefanofabi96@gmail.com>
2025-11-02 22:22:00 -0300

    Fix discounts

--- resources/views/pages/home/cards/payment_methods.blade.php
+++ resources/views/pages/home/cards/payment_methods.blade.php
@@ -11,7 +11,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet" viewBox="0 0 16 16">
                 <path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z"/>
               </svg> 
-              Billeteras virtuales <span class="badge text-bg-danger"> 10% descuento </span> 
+              Billeteras virtuales <span class="badge text-bg-danger"> 5% descuento </span> 
             </li>
 
             <li class="list-group-item"> 
@@ -21,7 +21,7 @@
               <a class="text-decoration-none text-dark" href="{{ asset('transferencia-electronica-de-fondos.pdf') }}" target="_blank"> 
                 Transferencia CBU 
               </a> 
-              <span class="badge text-bg-danger"> 10% descuento </span> 
+              <span class="badge text-bg-danger"> 5% descuento </span> 
             </li>
             
             <li class="list-group-item"> 
@@ -65,7 +65,7 @@
               </svg>
                 Débito automático 
               </a>
-              <span class="badge text-bg-danger"> 20% descuento </span>  
+              <span class="badge text-bg-danger"> 10% descuento </span>  
             </li>
 
             <li class="list-group-item"> 
