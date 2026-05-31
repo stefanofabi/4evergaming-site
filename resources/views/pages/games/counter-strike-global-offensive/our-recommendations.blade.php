da05186c933e094d10b713ef2b95fd51b5ebb138
Stefano <stefanofabi96@gmail.com>
2026-05-31 14:38:51 -0300

    Change pricing from 64 tickrate to 128 tickrate

--- resources/views/pages/games/counter-strike-global-offensive/our-recommendations.blade.php
+++ resources/views/pages/games/counter-strike-global-offensive/our-recommendations.blade.php
@@ -9,7 +9,7 @@
           <h3 class="card-title fw-bold"> Mini </h3>
           <p class="card-text"> Un paquete más pequeño para divertirse y jugar con amigos con tranquilidad. </p>
           <p class="card-text"> Jugadores: 8 </p>
-          <p class="card-text fs-4"> <strong> ${{ $slot_64tickrate_price * $dollar_price * 8 }} </strong> / mes </p>
+          <p class="card-text fs-4"> <strong> ${{ $slot_128tickrate_price * $dollar_price * 8 }} </strong> / mes </p>
         </div>
         <div class="card-footer text-center bg-danger fs-5" style="--bs-bg-opacity: .8;">
           <a href="https://clientes.4evergaming.com.ar/store/counter-strike-go/counter-strike-global-offensive-64-tickrate?currency=2" target="_blank" class="text-decoration-none text-dark fw-bold text-uppercase"> EMPEZAR </a>
@@ -39,7 +39,7 @@
           <h3 class="card-title fw-bold"> Comunidad</h3>
           <p class="card-text">Hecho a medida para grandes servidores comunitarios como DM, Surf y más.</p>
           <p class="card-text"> Jugadores: 20 </p>
-          <p class="card-text fs-4"> <strong> ${{ $slot_64tickrate_price * $dollar_price * 20 }} </strong> / mes </p>
+          <p class="card-text fs-4"> <strong> ${{ $slot_128tickrate_price * $dollar_price * 20 }} </strong> / mes </p>
         </div>
 
         <div class="card-footer text-center bg-danger fs-5" style="--bs-bg-opacity: .8;">
@@ -52,12 +52,12 @@
       <div class="card">
         <div class="card-body">
           <h3 class="card-title fw-bold"> Personalizado</h3>
-          <p class="card-text"> Cree su propia configuración. Seleccione la capacidad del servidor, tickrate y el precio. </p>
-          <p class="card-text fs-4"> Desde <br /> <strong> ${{ $slot_64tickrate_price * $dollar_price }} </strong> / slot </p>
+          <p class="card-text"> Tu propia configuración. Seleccioná la capacidad del servidor acorde a tu necesidad </p>
+          <p class="card-text fs-4"> Desde <br /> <strong> ${{ $slot_128tickrate_price * $dollar_price }} </strong> / slot </p>
         </div>
 
         <div class="card-footer text-center bg-danger fs-5" style="--bs-bg-opacity: .8;">
-          <a href="https://clientes.4evergaming.com.ar/store/counter-strike-go?currency=2" target="_blank" class="text-decoration-none text-dark fw-bold text-uppercase"> EMPEZAR </a>
+          <a href="https://clientes.4evergaming.com.ar/store/counter-strike-go/counter-strike-global-offensive-64-tickrate?currency=2" target="_blank" class="text-decoration-none text-dark fw-bold text-uppercase"> EMPEZAR </a>
         </div>
       </div>
     </div>
