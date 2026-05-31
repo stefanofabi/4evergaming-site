be5b434bfb8b95c6d3f4ce369c541cd20d617b13
Stefano <stefanofabi96@gmail.com>
2026-05-31 14:47:22 -0300

    Add multi-protocol support FAQ for Counter-Strike

---
+++ resources/views/pages/games/counter-strike/accordion-frequently-questions/multi-protocol.blade.php
@@ -0,0 +1,21 @@
+<div class="accordion-item">
+    <h2 class="accordion-header" id="headingSteam">
+      <button class="accordion-button collapsed" type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseSteam"
+              aria-expanded="false"
+              aria-controls="collapseSteam">
+        ¿Los servidores son compatibles con Steam y No-Steam?
+      </button>
+    </h2>
+    <div id="collapseSteam"
+         class="accordion-collapse collapse"
+         aria-labelledby="headingSteam"
+         data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>
+          Sí. Todos nuestros servidores de Counter-Strike cuentan con soporte multiprotocolo, permitiendo la conexión de jugadores tanto desde clientes Steam como No-Steam.
+        </p>
+      </div>
+    </div>
+</div>
