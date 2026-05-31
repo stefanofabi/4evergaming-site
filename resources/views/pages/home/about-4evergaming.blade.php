f49f6a0b329569da4aa653c9056f7ee783c5514f
Stefano <stefanofabi96@gmail.com>
2026-05-31 14:07:17 -0300

    Revise about page for 4evergaming
    Updated content and structure for the 4evergaming about page, enhancing community engagement and service descriptions.

--- resources/views/pages/home/about-4evergaming.blade.php
+++ resources/views/pages/home/about-4evergaming.blade.php
@@ -1,50 +1,76 @@
-  <div class="text-center mt-5">
-    <a class="text-decoration-none text-dark" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Conocé más sobre 4evergaming ⬇️</a>
-  </div>
+<div class="text-center mt-5">
+    <a class="text-decoration-none text-dark" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">
+        Conocé más sobre 4evergaming ⬇️
+    </a>
+</div>
 
-  <div class="row collapse" id="multiCollapseExample1">
+<div class="row collapse" id="multiCollapseExample1">
     <div class="row">
-      <div class="col-md bg-secondary bg-gradient ms-2 me-2 pt-4 pb-4 ps-4 pe-4 mt-2" style="--bs-bg-opacity: .15;">
-        <svg class="text-danger ps-2 pb-2 pt-2 pe-2" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-pc-display-horizontal" viewBox="0 0 16 16">
-          <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v7A1.5 1.5 0 0 0 1.5 10H6v1H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5v-1h4.5A1.5 1.5 0 0 0 16 8.5v-7A1.5 1.5 0 0 0 14.5 0h-13Zm0 1h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5ZM12 12.5a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0Zm2 0a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0ZM1.5 12h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1ZM1 14.25a.25.25 0 0 1 .25-.25h5.5a.25.25 0 1 1 0 .5h-5.5a.25.25 0 0 1-.25-.25Z"/>
-        </svg> <span> Hola Mundo!👋 Soy Stéfano</span>
-        <h2 class="mt-3"> Amo los jueguitos de computadoras 😍 </h2>
-          
-        <p class="mt-2">
-          Soy un aficionado que en el Año 2014 me decidí, junto a mi hermano en aquel entonces, a emprender con Game Hosting para ayudar a las personas a crear de manera fácil y rápida su propia Comunidad.
-          Actualmente reparto mi tiempo con mis estudios relacionados a la Informática 👨‍💻, entrenamiento 🏃‍♂️🏋️ y los GP de la F1 🏎️.  
-          Ya te lo estabas preguntando! Obvio que Team Red Bull 😎🏆🏆, aunque tengo mis respetos por Hamilton y a la Scuderia de Ferrari.
-          <br /><br />
-          Un gusto tenerte por acá! Te espero!😊
-        </p>
-      </div>
+        <div class="col-md bg-secondary bg-gradient ms-2 me-2 pt-4 pb-4 ps-4 pe-4 mt-2" style="--bs-bg-opacity: .15;">
+            <svg class="text-danger ps-2 pb-2 pt-2 pe-2" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 0a8 8 0 1 0 8 8A8 8 0 0 0 8 0Zm0 1a7 7 0 1 1-7 7A7 7 0 0 1 8 1Zm0 2a.5.5 0 0 1 .5.5V8h3.5a.5.5 0 0 1 0 1H8a.5.5 0 0 1-.5-.5V3.5A.5.5 0 0 1 8 3Z"/>
+            </svg>
+
+            <span>Nuestra visión </span>
+
+            <h2 class="mt-3">Impulsando comunidades gaming</h2>
+
+            <p class="mt-2">
+                En 4evergaming desarrollamos soluciones de hosting y servicios para comunidades de videojuegos,
+                combinando infraestructura de alto rendimiento con una atención cercana y personalizada.
+
+                <br><br>
+
+                Nuestro objetivo es brindar una plataforma confiable que permita a administradores, creadores
+                y jugadores enfocarse en lo que realmente importa: construir experiencias, crear comunidades
+                y disfrutar del juego.
+
+                <br><br>
+
+                La innovación tecnológica, la estabilidad y el compromiso con nuestros clientes son los pilares
+                que nos impulsan a seguir evolucionando día a día.
+            </p>
+        </div>
     </div>
 
     <div class="row mt-2">
-      <div class="col-md bg-secondary bg-gradient ms-2 me-2 pt-4 pb-4 ps-4 pe-4 mt-2" style="--bs-bg-opacity: .15;">
-        <svg class="text-danger ps-2 pb-2 pt-2 pe-2" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5Z"/>
-        </svg> <span> Récord de jugadores en línea</span>
-        <h2 class="mt-3"> +15000 Jugadores simultáneos </h2>
-          
-        <p class="mt-2"> 
-          El día 28/03/2020 se registró el mayor caudal de usuarios en nuestros servidores. 
-          Me puse muy contento de llegar a este número y de estar siempre en sus momentos de mayor diversión conectados.
-          Nuestra red llegó a soportar más de 10G de conexión en un momento de crisis imprevisible donde todas las personas se encontraban jugando en internet a toda hora.
-        </p>
-      </div>
+        <div class="col-md bg-secondary bg-gradient ms-2 me-2 pt-4 pb-4 ps-4 pe-4 mt-2" style="--bs-bg-opacity: .15;">
+            <svg class="text-danger ps-2 pb-2 pt-2 pe-2" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5Z"/>
+            </svg>
 
-      <div class="col-md bg-secondary bg-gradient ms-2 me-2 pt-4 pb-4 ps-4 pe-4 mt-2" style="--bs-bg-opacity: .15;">
-        <svg class="text-danger ps-2 pb-2 pt-2 pe-2" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-bookmark-star" viewBox="0 0 16 16">
-          <path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z"/>
-          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
-        </svg> <span> Reconocimiento por elegirnos </span>
-        <h2 class="mt-3"> +20000 Comunidades nos eligen </h2>
-        
-        <p class="mt-2"> 
-          Logré relaciones dinámicas con todos los clientes y siempre me adapté a sus necesidades. Los supe escuchar, reducí sus tiempos de espera y también aprendí mucho de ellos. Creo que son las claves de mi éxito.
-          Recompenso aquellos clientes con nuestro programas de fidelización para dar las gracias por seguir apoyandome en mi emprendimiento. 
-        </p>
-      </div>
+            <span>Récord de jugadores en línea</span>
+
+            <h2 class="mt-3">+15000 Jugadores simultáneos</h2>
+
+            <p class="mt-2">
+                El día 28/03/2020 se registró el mayor caudal de usuarios en nuestros servidores.
+                Llegamos a superar los 15.000 jugadores conectados simultáneamente, consolidando uno
+                de los momentos más importantes de nuestra historia.
+
+                Nuestra infraestructura logró soportar más de 10G de tráfico en un contexto donde
+                millones de personas elegían los videojuegos como forma de entretenimiento y conexión.
+            </p>
+        </div>
+
+        <div class="col-md bg-secondary bg-gradient ms-2 me-2 pt-4 pb-4 ps-4 pe-4 mt-2" style="--bs-bg-opacity: .15;">
+            <svg class="text-danger ps-2 pb-2 pt-2 pe-2" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z"/>
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+            </svg>
+
+            <span>Reconocimiento por elegirnos</span>
+
+            <h2 class="mt-3">+20000 Comunidades nos eligen</h2>
+
+            <p class="mt-2">
+                A lo largo de los años hemos construido relaciones duraderas con miles de clientes,
+                adaptándonos a las necesidades de cada proyecto y brindando un soporte cercano y eficiente.
+
+                Escuchar a nuestra comunidad, reducir tiempos de respuesta y mantener una mejora continua
+                han sido factores clave para nuestro crecimiento. Por eso seguimos recompensando la confianza
+                de quienes eligen 4evergaming día tras día.
+            </p>
+        </div>
     </div>
-  </div>
+</div>
